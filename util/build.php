b58f88c8cbdd4f061c31c5f20569f3cfe9be9fff
Craig Campbell <iamcraigcampbell@gmail.com>
2010-09-28 23:00:05 -0400

    build file now sets the @version param in all files and some more stuff for the core file

--- util/build.php
+++ util/build.php
@@ -5,7 +5,7 @@
  *
  * @author Craig Campbell
  */
-$lib_path = str_replace('/util/make_core.php', '/lib/Sonic', realpath(__FILE__));
+$lib_path = str_replace('/util/build.php', '/lib/Sonic', realpath(__FILE__));
 
 // files to combine
 $combine = array('App.php',
@@ -28,18 +28,26 @@ function getPath($lib_path, $file)
     return $lib_path . '/' . $file;
 }
 
+/**
+ * adds a version number to a lib file
+ *
+ * @param string $path path to file
+ * @param string $version
+ * @return void
+ */
+function addVersion($path, $version)
+{
+    $contents = file_get_contents($path);
+    $contents = preg_replace("/\* @version(.*)\n/", '* @version ' . $version . "\n", $contents);
+    file_put_contents($path, $contents);
+}
+
 // remove existing core file
 if (file_exists($lib_path . '/Core.php')) {
     unlink($lib_path . '/Core.php');
 }
 
-$minimize = in_array('--minimize', $_SERVER['argv']);
-
-$version = null;
-$key = array_search('--version', $_SERVER['argv']);
-if ($key !== false && isset($_SERVER['argv'][$key + 1])) {
-    $version = $_SERVER['argv'][$key + 1];
-}
+$minimize = !in_array('--no-minimize', $_SERVER['argv']);
 
 // figure out the last revision
 shell_exec('cd ' . $lib_path);
@@ -47,7 +55,7 @@ $revision = shell_exec('git log | head -1');
 $revision_text = 'last commit: ' . str_replace(array("\n", 'commit '), '', $revision);
 
 // start output
-$output = "<?php\n/**\n * combined core files to speed up your application (with comments stripped)\n *\n * includes " . implode(', ', $combine) . "\n *\n * @author Craig Campbell\n *\n * " . $revision_text . "\n * generated: " . date('Y-m-d H:i:s') . " EST\n */\nnamespace Sonic;\n";
+$output = "<?php\n/**\n * combined core files to speed up your application (with comments stripped)\n *\n * includes " . implode(', ', $combine) . "\n *\n * @category Sonic\n * @package Core\n * @author Craig Campbell\n * @link http://www.sonicframework.com\n * @license http://www.apache.org/licenses/LICENSE-2.0.html\n * @version\n *\n * " . $revision_text . "\n * generated: " . date('Y-m-d H:i:s') . " EST\n */\nnamespace Sonic;\n";
 
 foreach ($combine as $file) {
     echo 'adding file ' . $file . "\n";
@@ -92,6 +100,19 @@ if ($minimize) {
 }
 
 file_put_contents($lib_path . '/Core.php', $output);
+
+$version = null;
+$key = array_search('--version', $_SERVER['argv']);
+if ($key !== false && isset($_SERVER['argv'][$key + 1])) {
+    $version = $_SERVER['argv'][$key + 1];
+}
+
+if ($version) {
+    $it = new RecursiveDirectoryIterator($lib_path);
+    foreach (new RecursiveIteratorIterator($it) as $file) {
+        addVersion($file, $version);
+    }
+}
 
 echo 'done' . "\n";
 exit(1);
