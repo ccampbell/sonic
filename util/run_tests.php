3557abed55fde1f500596d86fb698aaf31b1c10a
Craig Campbell <iamcraigcampbell@gmail.com>
2010-10-04 20:16:31 -0400

    catching exceptions in the run tests script

--- util/run_tests.php
+++ util/run_tests.php
@@ -9,4 +9,8 @@ use Sonic\UnitTest\Runner;
 $lib_path = str_replace('run_tests.php', '', realpath(__FILE__)) . '/../lib';
 include $lib_path . '/Sonic/UnitTest/Runner.php';
 
-Runner::start($_SERVER['argv']);
+try {
+    Runner::start($_SERVER['argv']);
+} catch (\Exception $e) {
+    echo $e->getMessage(),"\n";
+}
