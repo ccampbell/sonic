a288cc4148a2c7b4e13c2733015432bc51d1a832
Craig Campbell <iamcraigcampbell@gmail.com>
2010-10-16 18:38:19 -0400

    allowing support for other db drivers in the sync db script

--- util/sync_db.php
+++ util/sync_db.php
@@ -16,7 +16,8 @@ if (in_array('-h', $_SERVER['argv']) || in_array('--help', $_SERVER['argv'])) {
     echo "arguments: ","\n";
     echo "--dry-run         outputs the sql of the changes since the last sync","\n";
     echo "                  does not actually run the sql","\n";
-    echo "--no-pdo          use mysql_query instead of PDO","\n";
+    echo "--mysql           use mysql instead of PDO","\n";
+    echo "--mysqli          use mysqli instead of PDO","\n";
     echo "-v,--verbose      show verbose output","\n";
     echo "-h,--help         shows this menu","\n";
     exit;
@@ -27,8 +28,12 @@ $app = App::getInstance();
 $app->addSetting(App::AUTOLOAD, true);
 
 // if we would prefer mysql_query over pdo
-if (in_array('--no-pdo', $_SERVER['argv'])) {
-    $app->addSetting(App::FAKE_PDO, true);
+if (in_array('--mysql', $_SERVER['argv'])) {
+    $app->addSetting(App::DB_DRIVER, App::MYSQL);
+}
+
+if (in_array('--mysqli', $_SERVER['argv'])) {
+    $app->addSetting(App::DB_DRIVER, APP::MYSQLI);
 }
 
 $app->start(App::COMMAND_LINE);
