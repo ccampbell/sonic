31e4fe869590b68f6dcfe7d61703adb52801ce2e
Craig Campbell <iamcraigcampbell@gmail.com>
2011-03-04 22:03:24 -0500

    update Config tests to take into account new options

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -12,7 +12,7 @@ class ConfigTest extends TestCase
         $this->isTrue($config instanceof Config);
 
         // no parents
-        $config = new Config($app->getPath('configs') . '/app2.ini', 'global');
+        $config = new Config($app->getPath('configs') . '/simple.ini', 'global');
         $this->isTrue($config instanceof Config);
 
         // test php based config
@@ -33,7 +33,21 @@ class ConfigTest extends TestCase
         $config = new Config('/bad/path', App::getInstance()->getEnvironment());
     }
 
-    public function testGet()
+    public function testGetAll()
+    {
+        $app = App::getInstance();
+        $path = $app->getPath('configs') . '/app.ini';
+
+        $config = new Config($path, 'production');
+        $all = $config->getAll();
+        $this->isEqual(count($all), 4);
+        $this->isTrue(isset($all['evil_users']));
+        $this->isTrue(isset($all['use_analytics']));
+        $this->isTrue(isset($all['urls']));
+        $this->isTrue(isset($all['debug']));
+    }
+
+    public function testRegularConfig()
     {
         $app = App::getInstance();
         $path = $app->getPath('configs') . '/app.ini';
@@ -73,6 +87,51 @@ class ConfigTest extends TestCase
 
         $url = $config->get('urls', 'test');
         $this->isEqual($url, 'http://www.website.com/test');
+
+        $analytics = $config->get('use_analytics');
+        $this->isEqual($analytics, 0);
+    }
+
+    public function testSmartConfig()
+    {
+        $app = App::getInstance();
+        $path = $app->getPath('configs') . '/smart.ini';
+
+        $config = new Config($path, 'production');
+        $var = $config->get('made_up_var');
+        $this->isNull($var);
+
+        $analytics = $config->get('use_analytics');
+        $this->isEqual($analytics, 1);
+
+        $debug = $config->get('debug');
+        $this->isEqual($debug, 0);
+
+        $evil_users = $config->get('evil_users');
+        $this->isEqual(count($evil_users), 1);
+
+        $url = $config->get('urls', 'www');
+        $this->isEqual($url, 'http://www.website.com');
+
+        $url = $config->get('urls', 'test');
+        $this->isEqual($url, 'http://www.testwebsite.com');
+
+        $config = new Config($path, 'user');
+        $debug = $config->get('debug');
+        $this->isEqual($debug, 1);
+
+        $new_array = $config->get('new_array');
+        $this->isEqual($new_array, array(25));
+
+        $url = $config->get('urls', 'www');
+        $this->isEqual($url, 'http://user.website.local');
+
+        $urls = $config->get('urls');
+        $this->isArray($urls);
+        $this->isEqual(count($urls), 1);
+
+        $url = $config->get('urls', 'test');
+        $this->isEqual($url, null);
 
         $analytics = $config->get('use_analytics');
         $this->isEqual($analytics, 0);
