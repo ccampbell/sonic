e175af5a3c6fb71435d828b208490e6a5450100f
Craig Campbell <iamcraigcampbell@gmail.com>
2011-03-13 01:29:14 -0500

    juggle some methods around and a few other minor tweaks

--- lib/Sonic/Controller.php
+++ lib/Sonic/Controller.php
@@ -106,6 +106,27 @@ class Controller
     }
 
     /**
+     * disables the view for this action
+     *
+     * @return void
+     */
+    public function disableView()
+    {
+        $this->getView()->disable();
+    }
+
+    /**
+     * disables the layout
+     *
+     * return Controller
+     */
+    public function disableLayout()
+    {
+        $this->_layout_name = null;
+        return $this;
+    }
+
+    /**
      * sets the view for this controller run
      *
      * @param string $name name of view to render
@@ -116,7 +137,7 @@ class Controller
     {
         // setting the view to the view we are already on
         if ($this->_view_name == $name) {
-            return $this;
+            return;
         }
 
         $this->_view_name = $name;
@@ -132,13 +153,22 @@ class Controller
         if (!$from_controller) {
             $this->_view = null;
         }
+    }
 
-        // if the layout is null that means it was disabled
-        if ($this->_layout_name === null) {
-            return $this;
+    /**
+     * gets the view object
+     *
+     * @return View
+     */
+    public function getView()
+    {
+        if ($this->_view === null) {
+            $this->_view = new View($this->getViewPath());
+            $this->_view->setAction($this->_view_name);
+            $this->_view->setActiveController($this->_name);
         }
 
-        return $this;
+        return $this->_view;
     }
 
     /**
@@ -153,6 +183,60 @@ class Controller
             $this->_request = $request;
         }
         return $this->_request;
+    }
+
+    /**
+     * returns a filter object to use for this
+     *
+     * @param string $name
+     * @return InputFilter
+     */
+    final public function filter($name)
+    {
+        if ($this->_input_filter !== null) {
+            return $this->_input_filter->filter($name);
+        }
+
+        App::getInstance()->includeFile('Sonic/InputFilter.php');
+        $this->_input_filter = new InputFilter($this->request());
+        return $this->_input_filter->filter($name);
+    }
+
+    /**
+     * sets layout
+     *
+     * @param string name of layout
+     * @return void
+     */
+    public function setLayout($name)
+    {
+        $this->_layout_name = $name;
+    }
+
+    /**
+     * gets layout
+     *
+     * @return Layout
+     */
+    public function getLayout()
+    {
+        if ($this->_layout === null) {
+            $layout_dir = App::getInstance()->getPath('views/layouts');
+            $layout = new Layout($layout_dir . '/' . $this->_layout_name . '.phtml');
+            $this->_layout = $layout;
+        }
+
+        return $this->_layout;
+    }
+
+    /**
+     * determines if this controller has a layout
+     *
+     * @return bool
+     */
+    public function hasLayout()
+    {
+        return $this->_layout_name !== null;
     }
 
     /**
@@ -189,64 +273,6 @@ class Controller
     }
 
     /**
-     * disables the layout
-     *
-     * return Controller
-     */
-    public function disableLayout()
-    {
-        $this->_layout_name = null;
-        return $this;
-    }
-
-    /**
-     * disables the view for this action
-     *
-     * @return void
-     */
-    public function disableView()
-    {
-        $this->getView()->disable();
-    }
-
-    /**
-     * determines if this controller has a layout
-     *
-     * @return bool
-     */
-    public function hasLayout()
-    {
-        return $this->_layout_name !== null;
-    }
-
-    /**
-     * sets layout
-     *
-     * @param string name of layout
-     * @return void
-     */
-    public function setLayout($name)
-    {
-        $this->_layout_name = $name;
-    }
-
-    /**
-     * gets layout
-     *
-     * @return Layout
-     */
-    public function getLayout()
-    {
-        if ($this->_layout === null) {
-            $layout_dir = App::getInstance()->getPath('views/layouts');
-            $layout = new Layout($layout_dir . '/' . $this->_layout_name . '.phtml');
-            $this->_layout = $layout;
-        }
-
-        return $this->_layout;
-    }
-
-    /**
      * gets the view path based on the view name
      *
      * @return string
@@ -254,22 +280,6 @@ class Controller
     final public function getViewPath()
     {
         return App::getInstance()->getPath('views') . '/' . $this->_name . '/' . $this->_view_name . '.phtml';
-    }
-
-    /**
-     * gets the view object
-     *
-     * @return View
-     */
-    public function getView()
-    {
-        if ($this->_view === null) {
-            $this->_view = new View($this->getViewPath());
-            $this->_view->setAction($this->_view_name);
-            $this->_view->setActiveController($this->_name);
-        }
-
-        return $this->_view;
     }
 
     /**
@@ -284,7 +294,7 @@ class Controller
             $this->getView()->addTurboData('redirect', $location);
             return;
         }
-        header('location: ' . $location);
+        $this->request()->setHeader('location', $location);
         exit;
     }
 
@@ -296,26 +306,9 @@ class Controller
      */
     protected function _json(array $data)
     {
-        header('Content-Type: application/json');
+        $this->request()->setHeader('Content-Type', 'application/json');
         echo json_encode($data);
         exit;
-    }
-
-    /**
-     * returns a filter object to use for this
-     *
-     * @param string $name
-     * @return InputFilter
-     */
-    final public function filter($name)
-    {
-        if ($this->_input_filter !== null) {
-            return $this->_input_filter->filter($name);
-        }
-
-        App::getInstance()->includeFile('Sonic/InputFilter.php');
-        $this->_input_filter = new InputFilter($this->request());
-        return $this->_input_filter->filter($name);
     }
 
     /**
