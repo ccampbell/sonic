b695249ebea5f75958a9327df820029a832b8da3
Craig Campbell <iamcraigcampbell@gmail.com>
2011-06-26 19:57:17 -0400

    manifest class updates
    add comments
    add a list of files to keep when extension is upgraded/deleted

--- lib/Sonic/Extension/Manifest.php
+++ lib/Sonic/Extension/Manifest.php
@@ -16,24 +16,45 @@ abstract class Manifest
     const VERSION = '';
 
     /**
+     * does this extension have a config file associated with it?
+     *
+     * if so it will be created in configs/extension.{extension_name}.ini
+     *
      * @var bool
      */
     protected $_has_config = false;
 
     /**
+     * optionally you can set some default configuration values
+     * to give your users an idea of what can be done
+     *
      * @var array
      */
     protected $_config_defaults = array();
 
     /**
+     * this is an array of dependencies for this extension
+     *
      * @var array
      */
     protected $_dependencies = array();
 
     /**
+     * additional routes that will be added to your configs/routes.php file
+     * when you install this extension
+     *
      * @var array
      */
     protected $_routes = array();
+
+    /**
+     * an array of files to keep on upgrade
+     * this is so if your extension installs a file and you upgrade the extension
+     * that file will not be overwritten
+     *
+     * @var array
+     */
+    protected $_keep_on_upgrade = array();
 
     /**
      * should we load the files in libs when the extension is first loaded in the app
@@ -43,6 +64,9 @@ abstract class Manifest
     protected $_load_libs = false;
 
     /**
+     * additional instructions to be displayed in command line after the extension
+     * has been successfully installed
+     *
      * @var string
      */
     protected $_instructions = '';
@@ -75,6 +99,16 @@ abstract class Manifest
     public function getConfigDefaults()
     {
         return $this->_config_defaults;
+    }
+
+    /**
+     * gets files to keep on upgrade
+     *
+     * @return array
+     */
+    public function keepOnUpgrade()
+    {
+        return $this->_keep_on_upgrade;
     }
 
     /**
