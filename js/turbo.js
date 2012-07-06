053caaa09ed1dd14005f8991ba6b2a03776a124c
gordyr <gordonrankin82@gmail.com>
2012-07-06 13:57:07 +0200

    Add simple resource dependency management and resolve CSS flashing issue for fragments

--- js/turbo.js
+++ js/turbo.js
@@ -1,3 +1,4 @@
+
 /**
  * class for handling injecting JSON views rendered into the DOM
  *
@@ -12,6 +13,8 @@ window.SonicTurbo = (function(doc)
      * @var array
      */
     var _js_queue = [];
+    var loadedjs = [];
+    var loadedcss = [];
 
     /**
      * adds an array of css files to the document
@@ -19,9 +22,12 @@ window.SonicTurbo = (function(doc)
      * @param JSON
      * @return void
      */
-    function _addCss(css) {
+    function _addCss(css, fragment) {
         for (var i = 0; i < css.length; ++i) {
-            _addCssFile(css[i]);
+    	if(inArray(loadedcss, css[i])){
+			_addCssFile(css[i], fragment);
+			loadedcss.push(css[i]); // flag css file as 'loaded'
+		}
         }
     }
 
@@ -31,13 +37,38 @@ window.SonicTurbo = (function(doc)
      * @param string
      * @return void
      */
-    function _addCssFile(filename) {
-        var stylesheet = doc.createElement("link");
-        stylesheet.setAttribute("rel", "stylesheet");
-        stylesheet.setAttribute("type", "text/css");
-        stylesheet.setAttribute("href", filename);
-        doc.getElementsByTagName("head")[0].appendChild(stylesheet);
+    function _addCssFile(filename, fragment) {
+	
+	
+		var stylesheet = doc.createElement("link");
+		stylesheet.setAttribute("rel", "stylesheet");
+		stylesheet.setAttribute("type", "text/css");
+		stylesheet.setAttribute("href", filename);
+		doc.getElementsByTagName("head")[0].appendChild(stylesheet);
+	
+	stylesheet.onload = function(){
+		fragment.setAttribute("style", "visibility:visible;");
+	}
+	
+        
     }
+    
+	/**
+	 * Checks loaded JS/CSS files
+	 *
+	 */
+    
+	function inArray(array, filename){
+		
+		for(var i=0;i<array.length;i++) {
+			if(array[i] == filename) {
+				return false;
+			}	
+		}
+		return true;	
+	}
+    
+    
 
     /**
      * loads the next javascript file from the queue
@@ -46,7 +77,10 @@ window.SonicTurbo = (function(doc)
      */
     function _processQueue() {
         if (_js_queue.length) {
-            _addJsFile(_js_queue[0]);
+		if(inArray(loadedjs, _js_queue[0])){
+			_addJsFile(_js_queue[0]);
+			loadedjs.push(_js_queue[0]); // flag js file as 'loaded'
+		}
         }
     }
 
@@ -60,7 +94,7 @@ window.SonicTurbo = (function(doc)
         var body = doc.getElementsByTagName("body")[0],
             script = doc.createElement("script"),
             done = false;
-
+	    
         script.src = filename;
 
         script.onload = script.onreadystatechange = function() {
@@ -90,9 +124,11 @@ window.SonicTurbo = (function(doc)
          */
         init : function()
         {
-            doc.cookie = 'noturbo=; expires=Thu, 01-Jan-70 00:00:01 GMT;';
+            //doc.cookie = 'noturbo=; expires=Thu, 01-Jan-70 00:00:01 GMT;';
         },
-
+	
+	
+	
         /**
          * public function to render a view
          *
@@ -101,16 +137,22 @@ window.SonicTurbo = (function(doc)
          */
         render: function(data)
         {
+		 var fragment = doc.getElementById(data.id);
+		     fragment.setAttribute("style", "visibility:hidden;");
+		
             // called via $this->_redirect() in a controller
             if (data.redirect) {
                 window.location = data.redirect;
             }
-            _addCss(data.css);
+	    fragment.innerHTML = data.content;
+            _addCss(data.css, fragment);
             doc.title = data.title;
-            doc.getElementById(data.id).innerHTML = data.content;
+	    
+	    
+            
 
-            for (var i in data.js) {
-                _js_queue.push(data.js[i]);
+            for (var i in data.js) {		
+			 _js_queue.push(data.js[i]);
             }
 
             _processQueue();
@@ -124,3 +166,5 @@ if (window.addEventListener) {
 else {
     window.attachEvent('onload', SonicTurbo.init);
 }
+
+
